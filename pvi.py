2a8a0a4e6a3b84edeefb009e93f48208571d8d46
iostreamer-X <iostreamer007@gmail.com>
2016-01-02 02:42:44 +0530

    Moved to soundcloud

--- pvi.py
+++ pvi.py
@@ -7,11 +7,12 @@ import os
 
 title = sys.argv[-1]
 print(title)
-query = urllib.urlencode({'q' : title+'youtube'})
+query = urllib.urlencode({'q' : title+'soundcloud'})
 url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' \
 % (query)
 search_results = urllib.urlopen(url)
 json = json.loads(search_results.read())
 results = json['responseData']['results']
 link = results[0]['url']
+print link
 os.system('google-chrome '+link)
